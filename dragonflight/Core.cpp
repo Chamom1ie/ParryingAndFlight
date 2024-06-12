035713e6c7deec15b7e99401ff0a2ad985a7d0e4
Chamom1ie <GitHubmile@gmail.com>
2024-06-12 15:12:06 +0900

    aa

--- dragonflight/Core.cpp
+++ dragonflight/Core.cpp
@@ -1,11 +1,14 @@
 #include "Core.h"
 #include "Console.h"
+#include "TitleScene.h"
 Core* Core::m_pInst = nullptr;
 bool Core::Init() //여기서 게임 진행?
 {
-	Console console{};
-	console.Init();
-	system("pause");
+	Console m_console{};
+	TitleScene m_titleScene{};
+	m_console.Init();
+	m_titleScene.Run();
+
 	//초기화
 	//MapMgr::GetInst()->Init();
 	//ObjectMgr::GetInst()->Init();
